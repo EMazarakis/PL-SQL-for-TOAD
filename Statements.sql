72f096682fa4aa039d0aa1ba61b0811e5b895ce9
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-09-17 15:52:44 +0300

    SQL Statements for TOAD

---
+++ Statements.sql
@@ -0,0 +1,9 @@
+
+SELECT * FROM table_name;   --- For the selected table return  all the records (*)
+
+SELECT column1  FROM table_name; --- Return only the column1 field for all records of the table
+
+SELECT  column1, column2  FROM table_name; ---  Return only the column1 & column2  fields for all records of the table
+
+SELECT column1 AS "newNameOfTheColumn1" FROM table_name; --- Return only the column1 field for all records of the table but results displaying with the column name newNameOfTheColumn1
+                                                         ---- If you want to rename the title of the  displaying results
