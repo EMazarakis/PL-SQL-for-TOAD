37f8df1e71d4ba9dff586a6572eeb148317392bc
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-09-18 13:57:24 +0300

    Update Statements.sql

--- Statements.sql
+++ Statements.sql
@@ -37,18 +37,3 @@ SELECT column1, column2 FROM table_name WHERE column3 >= 'searchValue';
 --- Return the values of the column1, column2 when column3 (field) has value different than 'searchValue'
 SELECT column1, column2 FROM table_name WHERE column3 <> 'searchValue';
 SELECT column1, column2 FROM table_name WHERE column3 != 'searchValue';
-
-
----- Searching for patterns into a field with LIKE operator and  wildcards % , _ 
---- % : instead of too many characters (recognizable by TOAD)
---- _ : instead of one character (recognizable by TOAD)
-
---- Return the column1, column2 for the records where column3 has the pattern whatever characters ended with aabra_cada_bra text
-SELECT column1, column2 FROM table_name WHERE column3 LIKE '%aabra_cada_bra';
-
---- Return the column1, column2 for the records where column3 has the pattern starts with A whatever character and ends with S
-SELECT column1, column2 FROM table_name WHERE column3 LIKE 'A_S';
-
---- Return the column1, column2 for the records where column3 starts with 1 whatever characters and ends with 1
-SELECT column1, column2 FROM table_name WHERE column3 LIKE '1%1';
-
