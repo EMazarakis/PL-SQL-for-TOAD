126dafb6236aeb1783ee9884504172f0d99c15a1
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-09-18 13:59:16 +0300

    Update Statements.sql

--- Statements.sql
+++ Statements.sql
@@ -13,27 +13,3 @@ SELECT column1 AS "newNameOfTheColumn1" FROM table_name;
 
 --- Return the disctinct values of the column1 field
 SELECT DISTINCT colums1 FROM table_name;
-                                                      
-                                                      
----- WHERE : the fields of the SELECT part are returned when the WHERE part is TRUE.
----- Compare Characters: 'A' < 'B' < 'C' < 'D' < .... 
----- Compare Numbers:
-
---- Return the values of the column1, column2 when column3 (field) has value equal to 'searchValue'                                 
-SELECT column1, column2 FROM table_name WHERE column3 = 'searchValue';
-
---- Return the values of the column1, column2 when column3 (field) has value less than 'searchValue'
-SELECT column1, column2 FROM table_name WHERE column3 < 'searchValue';
-
---- Return the values of the column1, column2 when column3 (field) has value less than OR equal to 'searchValue'
-SELECT column1, column2 FROM table_name WHERE column3 <= 'searchValue';
-
---- Return the values of the column1, column2 when column3 (field) has value bigger than 'searchValue'
-SELECT column1, column2 FROM table_name WHERE column3 > 'searchValue';
-
---- Return the values of the column1, column2 when column3 (field) has value bigger than OR equal to 'searchValue'
-SELECT column1, column2 FROM table_name WHERE column3 >= 'searchValue';
-
---- Return the values of the column1, column2 when column3 (field) has value different than 'searchValue'
-SELECT column1, column2 FROM table_name WHERE column3 <> 'searchValue';
-SELECT column1, column2 FROM table_name WHERE column3 != 'searchValue';
