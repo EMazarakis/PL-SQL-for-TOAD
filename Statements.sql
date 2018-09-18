070c30cc53bd213af2ec260e32ac9c4ad6b9d0cf
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-09-18 13:01:49 +0300

    Update Statements.sql

--- Statements.sql
+++ Statements.sql
@@ -1,9 +1,34 @@
-SELECT * FROM table_name;   --- For the selected table return  all the records (*)
+--- For the selected table return  all the records (*)
+SELECT * FROM table_name;   
 
-SELECT column1  FROM table_name; --- Return only the column1 field for all records of the table
+--- Return only the column1 field for all records of the table
+SELECT column1  FROM table_name; 
 
-SELECT  column1, column2  FROM table_name; ---  Return only the column1 & column2  fields for all records of the table
+---  Return only the column1 & column2  fields for all records of the table
+SELECT  column1, column2  FROM table_name; 
 
-SELECT column1 AS "newNameOfTheColumn1" FROM table_name; --- Return only the column1 field for all records of the table but results displaying with the column name newNameOfTheColumn1
-                                                         ---- If you want to rename the title of the  displaying results
+--- Return only the column1 field for all records of the table but results displaying with the column name newNameOfTheColumn1
+--- If you want to rename the title of the  displaying results
+SELECT column1 AS "newNameOfTheColumn1" FROM table_name; 
+
+--- Return the disctinct values of the column1 field
+SELECT DISTINCT colums1 FROM table_name;
                                                       
+                                                      
+---- WHERE : the fileds of the SELECT part are returned when the WHERE part is TRUE.   
+
+
+--- Return the values of the column1, column2 when column3 (field) has value equal to 'searchValue'                                 
+SELECT column1, column2 FROM table_name WHERE column3 = 'searchValue';
+
+--- Return the values of the column1, column2 when column3 (field) has value less than 'searchValue'
+SELECT column1, column2 FROM table_name WHERE column3 < 'searchValue';
+
+--- Return the values of the column1, column2 when column3 (field) has value less than OR equal to 'searchValue'
+SELECT column1, column2 FROM table_name WHERE column3 <= 'searchValue';
+
+--- Return the values of the column1, column2 when column3 (field) has value bigger than 'searchValue'
+SELECT column1, column2 FROM table_name WHERE column3 > 'searchValue';
+
+--- Return the values of the column1, column2 when column3 (field) has value bigger than OR equal to 'searchValue'
+SELECT column1, column2 FROM table_name WHERE column3 >= 'searchValue';
