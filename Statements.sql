1404543ea5581afaa14100f1ebaf4025df690f6a
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-09-28 11:03:14 +0300

    Update Statements.sql

--- Statements.sql
+++ Statements.sql
@@ -14,8 +14,3 @@ SELECT column1 AS "newNameOfTheColumn1" FROM table_name;
 --- Return the disctinct values of the column1 field
 SELECT DISTINCT colums1 FROM table_name;
 
-
---- ORDER BY field [ASC | DESC]
-
-SELECT column1, column2 FROM table_name ORDER BY column3 ASC;   ---- Ascending Order
-SELECT column1, column2 FROM table_name ORDER BY column3 DESC;  ---- Descending Order
