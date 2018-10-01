8b03a59c0d34eaa4300d0590bd378f43e15227ed
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-10-01 12:26:38 +0300

    sql statements with BETWEEN operator.

--- BETWEEN_operator.sql
+++ BETWEEN_operator.sql
@@ -1,1 +1,6 @@
+--- Return all the records where  column3 has values in space [ value1, value2 ]
+SELECT column1, column2, FROM table_name WHERE column3 BETWEEN value1 AND value2;
 
+
+--- Return all the records where column3 has not values in [ value1, value2 ]
+SELECT column1, column2, FROM table_name WHERE column3 NOT BETWEEN value1 AND value2;
