d6ea45501d9b71436f2007bba10943fcfa7d2256
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-10-02 15:05:41 +0300

    Update EMBEDDED_Functions.sql

--- EMBEDDED_Functions.sql
+++ EMBEDDED_Functions.sql
@@ -1,2 +1,5 @@
 --- Return the sum for all the records of the field column1
 SELECT SUM(column1) AS "Sum of all column1's records" FROM table_name;
+
+--- Return the average value for all the records of the field column1
+SELECT AVG(column1) AS "Average of all column1's records" FROM table_name;
