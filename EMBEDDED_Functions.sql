a83e9b895ccf4f2d71aa506393c33fe0ed2c8f76
Mazarakis Eugene <mazarakiseug@gmail.com>
2018-10-08 12:22:47 +0300

    sum, avg, min, max,

--- EMBEDDED_Functions.sql
+++ EMBEDDED_Functions.sql
@@ -3,3 +3,9 @@ SELECT SUM(column1) AS "Sum of all column1's records" FROM table_name;
 
 --- Return the average value for all the records of the field column1
 SELECT AVG(column1) AS "Average of all column1's records" FROM table_name;
+
+--- Return the minimum value of the field column1
+SELECT MIN(column1)  FROM  table_name;
+
+--- Return the maximum value of the field column1
+SELECT MAX(column1)  FROM  table_name;
